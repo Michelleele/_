04df116548b8933aad6f474ff1b79473901fcfcb
BT_2S14_27 <email@email>
2024-09-16 10:11:15 -0400

    added print statement printing "boba"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Michelle");
+        System.out.println("Michelle");
+        System.out.println("boba");
         }
     }
